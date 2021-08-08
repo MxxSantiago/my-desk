65b5b1edf0787bdbdbeb62d9c969f24334f24045
Max Santiago <mxxsant@hotmail.com>
2021-08-08 16:40:13 -0700

    Added PWA support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainApp } from './components/MainApp';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <MainApp/>,
     document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
