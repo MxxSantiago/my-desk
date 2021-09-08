bd4354ccc7008bca024783ae9bfad36288402572
Max Santiago <mxxsant@hotmail.com>
2021-09-07 17:24:47 -0700

    feat: the calendar is now automatically set to the current day of the week

--- src/components/TimeListSection/SwiperNav.js
+++ src/components/TimeListSection/SwiperNav.js
@@ -1,35 +1,39 @@
-import React from 'react'
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from 'swiper/core';
+import SwiperCore, { Pagination } from "swiper/core";
 
 SwiperCore.use([Pagination]);
 
-export const SwiperNav = ({ setSwiperState }) => {
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-    const updateTimeList = ({ activeIndex }) => setSwiperState(days[activeIndex]);
+export const SwiperNav = ({ setSwiperState, days, todayIndex }) => {
+    const updateTimeList = ({ activeIndex }) =>
+        setSwiperState(days[activeIndex]);
 
     return (
         <div>
             <Swiper
+                initialSlide={`${todayIndex}`}
                 className="mySwiper fw-bold"
-                slidesPerView={'auto'}
+                slidesPerView={"auto"}
                 centeredSlides={true}
                 spaceBetween={30}
-                pagination={{ "clickable": true }}
-                onActiveIndexChange={swipeData => updateTimeList(swipeData)}
+                pagination={{ clickable: true }}
+                onActiveIndexChange={(swipeData) => updateTimeList(swipeData)}
             >
-                {
-                    days.map((day, index) => (
-                        <SwiperSlide
-                            key={index}
-                        >
-                            {({ isActive }) => (
-                                <div className={isActive ? "swipeActive fs-5" : "swipeInactive fs-6"}>{day}</div>
-                            )}
-                        </SwiperSlide>
-                    ))
-                }
+                {days.map((day, index) => (
+                    <SwiperSlide key={index}>
+                        {({ isActive }) => (
+                            <div
+                                className={
+                                    isActive
+                                        ? "swipeActive fs-5"
+                                        : "swipeInactive fs-6"
+                                }
+                            >
+                                {day}
+                            </div>
+                        )}
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
--- src/components/TimeListSection/TimeListSection.js
+++ src/components/TimeListSection/TimeListSection.js
@@ -1,30 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css"
-import { SwiperNav } from './SwiperNav';
-import { ScheduleAdd } from './ScheduleAdd';
-import { SchedulesList } from './SchedulesList';
+import "swiper/components/navigation/navigation.min.css";
+import { SwiperNav } from "./SwiperNav";
+import { ScheduleAdd } from "./ScheduleAdd";
+import { SchedulesList } from "./SchedulesList";
 
 export const TimeListSection = () => {
-    const schedulesInitialState = () => JSON.parse(localStorage.getItem('schedules')) || [];
+    const schedulesInitialState = () =>
+        JSON.parse(localStorage.getItem("schedules")) || [];
 
+    const days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
+
+    const todayIndex = [new Date().getDay() - 1];
     const [schedules, setSchedules] = useState(schedulesInitialState);
-    const [swiperState, setSwiperState] = useState("Monday");
-    
+    const [swiperState, setSwiperState] = useState(days[todayIndex]);
+
     useEffect(() => {
-        localStorage.setItem('schedules', JSON.stringify(schedules));
+        localStorage.setItem("schedules", JSON.stringify(schedules));
     }, [schedules]);
 
     return (
         <div className="timeList-section">
             <h1>Timelist</h1>
-            <hr/>
-            <SwiperNav setSwiperState={setSwiperState}/>
-            <hr/>
+            <hr />
+            <SwiperNav
+                setSwiperState={setSwiperState}
+                days={days}
+                todayIndex={todayIndex}
+            />
+            <hr />
             <h2 className="fs-5 mb-3">Add {swiperState} schedule</h2>
-            <ScheduleAdd setSchedules={setSchedules} swiperState={swiperState}/>
-            <hr/>
-            <SchedulesList schedules={schedules} setSchedules={setSchedules} swiperState={swiperState}/>
+            <ScheduleAdd
+                setSchedules={setSchedules}
+                swiperState={swiperState}
+            />
+            <hr />
+            <SchedulesList
+                schedules={schedules}
+                setSchedules={setSchedules}
+                swiperState={swiperState}
+            />
         </div>
     );
 };
