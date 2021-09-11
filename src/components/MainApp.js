552261b531497dd923c4b5fd07445547068e28e6
Max Santiago <mxxsant@hotmail.com>
2021-09-11 16:29:27 -0700

    feat: added new payload

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -19,6 +19,8 @@ export const MainApp = () => {
                         ? { ...todo, done: !todo.done }
                         : todo
                 );
+            case "import":
+                return action.payload;
             default:
                 return state;
         }
