68939e330244a803fe0b85bcd4968398c655af2d
Max Santiago <mxxsant@hotmail.com>
2021-09-11 16:28:00 -0700

    style: formatted code

--- src/components/HomeSection/HomeSection.js
+++ src/components/HomeSection/HomeSection.js
@@ -1,30 +1,39 @@
-import React, { useContext, useEffect } from 'react';
-import { UserContext } from '../UserContext';
-import { TodoAdd } from './TodoAdd';
-import { TodoList } from './TodoList';
+import React, { useContext, useEffect } from "react";
+import { UserContext } from "../UserContext";
+import { TodoAdd } from "./TodoAdd";
+import { TodoList } from "./TodoList";
 
 export const HomeSection = () => {
     const { todos, dispatch } = useContext(UserContext);
 
     useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
+        localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
 
-    const handleAddTodo = newTodo => dispatch({ type: 'add', payload: newTodo});
+    const handleAddTodo = (newTodo) =>
+        dispatch({ type: "add", payload: newTodo });
 
-    const handleDelete = todo => todo.done && dispatch({ type: 'delete', payload: todo.id });
+    const handleDelete = (todo) =>
+        todo.done && dispatch({ type: "delete", payload: todo.id });
 
-    const handleToggle = todoId => dispatch({ type: 'toggle', payload: todoId});
+    const handleToggle = (todoId) =>
+        dispatch({ type: "toggle", payload: todoId });
 
     return (
         <div>
             <h1>Tasks</h1>
-            <hr/>
-            <h2 className="fw-light mb-3 fs-6">Create a new task, choose a name and color.</h2>
-            <TodoAdd handleAddTodo={handleAddTodo}/>
-            <hr/>
+            <hr />
+            <h2 className="fw-light mb-3 fs-6">
+                Create a new task, choose a name and color.
+            </h2>
+            <TodoAdd handleAddTodo={handleAddTodo} />
+            <hr />
             <p>Tasks to do: {todos.length}</p>
-            <TodoList todos={todos} handleDelete={handleDelete} handleToggle={handleToggle} />
+            <TodoList
+                todos={todos}
+                handleDelete={handleDelete}
+                handleToggle={handleToggle}
+            />
         </div>
     );
 };
